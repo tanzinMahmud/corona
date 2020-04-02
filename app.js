417698461eaad26551bb898e13eb8a8c368b0644
Tanzin Mahmud <tanzinMahmud>
2020-04-02 11:00:16 +0600

    auto refresh added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function ($scope, $http) {
+
+    $scope.totalData = [];
+    $scope.allCountryData = [];
+
     $http.get('https://corona.lmao.ninja/all')
         .then(function (response) {
             $scope.totalData = response.data;
@@ -10,4 +14,16 @@ app.controller('myCtrl', function ($scope, $http) {
         .then(function (response) {
             $scope.allCountryData = response.data;
         });
+
+    setInterval(function () {
+        $http.get('https://corona.lmao.ninja/all')
+            .then(function (response) {
+                $scope.totalData = response.data;
+            });
+
+        $http.get('https://corona.lmao.ninja/countries?sort=country')
+            .then(function (response) {
+                $scope.allCountryData = response.data;
+            });
+    }, 3000);
 });
